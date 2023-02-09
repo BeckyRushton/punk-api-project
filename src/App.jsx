e767a50bd92c17a473e4a7d1085cde23b10fb351
Becky Rushton <beckyrushton30@gmail.com>
2023-02-09 15:44:18 +0000

    added files to folders and connected app to navbar and cardcontainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.scss";
 import beers from "./data/beers";
+import Navbar from "../src/containers/Navbar/Navbar";
+import CardContainer from "../src/containers/CardContainer";
+
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <CardContainer />
+    </div>
+  );
 };
 
 export default App;
