13bd0f124dc656eef51b1e5f9d76023ef120f235
Becky Rushton <beckyrushton30@gmail.com>
2023-02-15 16:54:39 +0000

    moved unnecessary code to code graveyard file

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Navbar from "../src/containers/Navbar/Navbar";
 import Main from "./containers/Main/Main";
 import React from "react";
 import { useState, useEffect } from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import BeerInfo from "./containers/BeerInfo/BeerInfo";
 
 const App = () => {
   const [beersAPI, setBeersAPI] = useState([]);
@@ -17,47 +15,45 @@ const App = () => {
     setBeersAPI(data);
   };
 
+  const getSearchBeers = async (searchTerm) => {
+    const url = `https://api.punkapi.com/v2/beers/?beer_name=${searchTerm}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    setBeersAPI(data);
+  };
+
+  // const getCheckedBeers = async () => {
+  //   const url = `https://api.punkapi.com/v2/beers/`;
+  //   const res = await fetch(url + `?abv_gt=6`);
+  //   const data = await res.json();
+  //   setBeersAPI(data);
+  // };
+
   useEffect(() => {
     getBeers();
   }, []);
 
   const handleSearch = (event) => {
     const searchTerm = event.target.value.toLowerCase();
-    const filteredTerm = beersAPI.filter((beer) => {
-      if (beer.name.toLowerCase().includes(searchTerm)) {
-        return true;
-      }
-    });
-    console.log(searchTerm);
-    setFilteredArr(filteredTerm);
-  };
-
-  const handleSelect = (event) => {
-    console.log(event.target.id);
-    if (event.target.id === "highABV" && event.target.checked) {
-      const filteredBeers = beersAPI.filter((beer) => {
-        return beer.abv > 6.0;
-      });
-      setFilteredArr(filteredBeers);
-    } else if (event.target.id === "acidic" && event.target.checked) {
-      const filteredBeers = beersAPI.filter((beer) => {
-        return beer.ph > 4.0;
-      });
-      setFilteredArr(filteredBeers);
-    } else if (event.target.id === "classicRange" && event.target.checked) {
-      const filteredBeers = beersAPI.filter((beer) => {
-        const beerArr = parseInt(beer.first_brewed.split("/")[1]);
-        return beerArr < 2010;
-      });
-      setFilteredArr(filteredBeers);
+    if (searchTerm) {
+      getSearchBeers(searchTerm);
     } else {
-      setFilteredArr(beersAPI);
+      getBeers();
     }
   };
+  // const handleSelect = (event) => {
+  //   if (event.target.id === "highABV") {
+  //     abv = true;
+  //   }
+  //   const abv = event.target.id === "highABV";
+  //   const ph = event.target.id === "acidic";
+  //   const classic = event.target.id === "classicRange";
+  //   getBeers();
+  // };
 
   return (
     <div className="app">
-      <Navbar handleSearch={handleSearch} handleSelect={handleSelect} />
+      <Navbar handleSearch={handleSearch} />
       <Main beers={beersAPI} />
     </div>
   );
